54fdbddde50f1c65c91318888b7fb5fe55c0b004
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-26 23:15:27 +0900

    add: テスト表示

--- pages/index.vue
+++ pages/index.vue
@@ -5,30 +5,28 @@
       <h1 class="title">
         nuxt-strapi-blog-frontend
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div v-for="article in articles" v-bind:key="article.id">
+        <h2>{{ article.title }}</h2>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { allArticlesQuery } from "~/graphql/queries";
+export default {
+  data() {
+    return {
+      articles: []
+    };
+  },
+  apollo: {
+    articles: {
+      prefetch: true,
+      query: allArticlesQuery
+    }
+  }
+};
 </script>
 
 <style>
@@ -42,16 +40,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
